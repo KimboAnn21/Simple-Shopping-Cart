806313430b0765d21e0ce12b64b7027aec0cd3a5
[Kim Grossman] <[kimgrossman21@gmail.com]>
2019-03-19 09:58:52 -0600

    App update

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
 
 export const Cart = ({items, removeFromCart}) => (
     <div>
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import App from './App.js';
 
 export const List = ({ addToCart, items }) => (
     <div>
